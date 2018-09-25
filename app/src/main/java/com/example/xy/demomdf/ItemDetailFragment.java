bcfdcd19d468d99836652514d4d1fe555348a001
Karan Acharya <karan7acharya@gmail.com>
2018-09-25 23:11:14 +0530

    Preliminary implementation of ExoPlayer

--- app/src/main/java/com/example/xy/demomdf/ItemDetailFragment.java
+++ app/src/main/java/com/example/xy/demomdf/ItemDetailFragment.java
@@ -1,9 +1,11 @@
 package com.example.xy.demomdf;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +14,25 @@ import android.widget.TextView;
 
 import com.example.xy.demomdf.data.RecipeData;
 import com.example.xy.demomdf.dummy.DummyContent;
-
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import java.util.ArrayList;
 
 /**
@@ -21,11 +41,16 @@ import java.util.ArrayList;
  * in two-pane mode (on tablets) or a {@link ItemDetailActivity}
  * on handsets.
  */
-public class ItemDetailFragment extends Fragment {
+public class ItemDetailFragment extends Fragment
+implements ExoPlayer.EventListener{
     /**
      * The fragment argument representing the item ID that this fragment
      * represents.
      */
+
+    private SimpleExoPlayerView exoPlayerView;
+    private SimpleExoPlayer exoPlayer;
+
     public static final String ARG_ITEM_ID = "item_id";
 
     public static final String ARG_STEPS_LIST_ID = "steps_list_id";
@@ -51,11 +76,9 @@ public class ItemDetailFragment extends Fragment {
 
         recipeSteps = new ArrayList<>();
 
-        if (getArguments().containsKey(ARG_STEPS_LIST_ID)){
+        if (getArguments().containsKey(ARG_STEPS_LIST_ID)) {
             recipeSteps = getArguments().getParcelableArrayList(ARG_STEPS_LIST_ID);
         }
-
-
 
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
@@ -74,6 +97,7 @@ public class ItemDetailFragment extends Fragment {
                 appBarLayout.setTitle(recipeSteps.get(Integer.valueOf(tag)).getShortDescription());
 
             }
+
         }
 
 
@@ -84,8 +108,9 @@ public class ItemDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
 
-        TextView videoUrlTextView = rootView.findViewById(R.id.video_url_tv);
         TextView mainDescriptionTextView = rootView.findViewById(R.id.item_detail);
+
+        exoPlayerView = rootView.findViewById(R.id.exo_player_view);
 
         // Show the dummy content as text in a TextView.
         /*
@@ -94,12 +119,86 @@ public class ItemDetailFragment extends Fragment {
         }
          */
 
-        if (recipeSteps != null  || recipeSteps.size() != 0){
+
+        if (recipeSteps != null || recipeSteps.size() != 0) {
             String videoUrl = recipeSteps.get(Integer.valueOf(tag)).getVideoUrl();
-            videoUrlTextView.setText(videoUrl);
+            if (videoUrl != null    || !videoUrl.equals(""))
+                setupExoPlayer(videoUrl);
             String mainDescription = recipeSteps.get(Integer.valueOf(tag)).getMainDescription();
             mainDescriptionTextView.setText(mainDescription);
         }
         return rootView;
+    }
+
+
+    private void setupExoPlayer(String videoURL){
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
+
+        Uri videoURI = Uri.parse(videoURL);
+
+        DefaultHttpDataSourceFactory dataSourceFactory =
+                new DefaultHttpDataSourceFactory("exoplayer_video");
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        MediaSource mediaSource = new ExtractorMediaSource(
+                videoURI,
+                dataSourceFactory,
+                extractorsFactory,
+                null,
+                null
+        );
+
+        exoPlayerView.setPlayer(exoPlayer);
+        exoPlayer.prepare(mediaSource);
+        exoPlayer.setPlayWhenReady(true);
+    }
+
+    private void releasePlayer(){
+        exoPlayer.stop();
+        exoPlayer.release();
+        exoPlayer = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
     }
 }
